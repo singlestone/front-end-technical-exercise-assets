59bdaf595a18a17d30a32dfa5451625fbaa9fe2a
David Leong <david0leong@gmail.com>
2020-12-30 08:29:25 -0800

    Add header

--- src/components/EnHeader.vue
+++ src/components/EnHeader.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="header">
-    Header
-  </div>
+  <header class="header">
+    <div class="container">
+      <logo-endless class="logo" />
+    </div>
+  </header>
 </template>
 
 <script>
+import logoEndless from '@/assets/images/logo-endless.svg'
+
 export default {
-  name: 'EnHeader'
+  name: 'EnHeader',
+  components: {
+    logoEndless
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  height: 52px;
+  background: #222;
+
+  .logo {
+    height: 21px;
+    fill: #50e3c2;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
-};
+  lintOnSave: false,
+
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+
+    svgRule.uses.clear()
+    svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+  }
+}
